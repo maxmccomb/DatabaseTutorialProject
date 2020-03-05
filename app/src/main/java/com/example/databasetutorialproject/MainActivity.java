b4f82b3f7cd94c0c86a52d7291bad0b33211c356
maxmccomb <maxwellmccomb16@augustana.edu>
2020-03-05 12:01:16 -0600

    Changed IDs

--- app/src/main/java/com/example/databasetutorialproject/MainActivity.java
+++ app/src/main/java/com/example/databasetutorialproject/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.databasetutorialproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -8,10 +9,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 /*
     To do this tutorial yourself go to: https://www.youtube.com/watch?v=EM2x33g4syY
@@ -23,7 +28,9 @@ public class MainActivity extends AppCompatActivity {
     EditText nameEntry;
     Spinner genreSpinner;
     Button addButton;
+    TextView t;
 
+    int zero = 0;
     DatabaseReference databaseArtists;
 
     @Override
@@ -36,10 +43,12 @@ public class MainActivity extends AppCompatActivity {
         nameEntry = findViewById(R.id.nameTextEdit);
         genreSpinner = findViewById(R.id.genreSpinner);
         addButton = findViewById(R.id.button);
+        t = findViewById(R.id.textView);
 
         addButton.setOnClickListener(new View.OnClickListener(){
             public void onClick(View view){
                 addArtist();
+                setText();
             }
         });
     }
@@ -49,11 +58,13 @@ public class MainActivity extends AppCompatActivity {
        String genre = genreSpinner.getSelectedItem().toString();
 
        if(!TextUtils.isEmpty(name)){
-            String id = databaseArtists.push().getKey();
+           zero ++;
+            String id = "" + zero;
 
             Artist artist = new Artist(name, genre, id);
 
             databaseArtists.child(id).setValue(artist);
+
 
            Toast.makeText(this, "Artist Added", Toast.LENGTH_LONG).show();
 
@@ -61,6 +72,22 @@ public class MainActivity extends AppCompatActivity {
        else{
            Toast.makeText(this, "Please enter a name", Toast.LENGTH_LONG).show();
        }
+    }
+
+    public void setText(){
+        DatabaseReference ref = databaseArtists.child("-M1cvbVHNmuvqojIO0Wh");
+        DatabaseReference ref2 = ref.child("artistName");
+        ref2.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                t.setText(""+dataSnapshot.getValue());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
 }
